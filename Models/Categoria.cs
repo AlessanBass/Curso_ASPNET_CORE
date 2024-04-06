020f4ec2b4f9921c189ee5061315cb3c526619f4
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-04-06 12:08:49 -0300

    Aplicando Data Annotacions

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,7 +1,13 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
+/* 
+    Aplicando Data Notations
+ */
 
+[Table("Categorias")] //não era necessário utilizar
 public class Categoria
 {
     /* É uma boa prática sempre incializar a coleção */
@@ -11,8 +17,15 @@ public class Categoria
     }
 
     /* Para chave primário ao usar EF usar nomes específicos */
+    [Key]
     public int CategoriaId {get; set;}
+
+    [Required]
+    [StringLength(80)]
     public string?  Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; } /* Armazena o caminho da string */
 
     /* Para relacionar, precisamos incluir uma propriedade de navegação que indica
