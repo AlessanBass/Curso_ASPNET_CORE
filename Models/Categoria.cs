c959d045c5a2aebfa99361951d1c32bf7fa1fc98
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-04-06 11:50:38 -0300

    Aplicando migrations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace APICatalogo.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models;
 
 public class Categoria
 {
+    /* É uma boa prática sempre incializar a coleção */
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
     /* Para chave primário ao usar EF usar nomes específicos */
     public int CategoriaId {get; set;}
     public string?  Nome { get; set; }
     public string? ImageUrl { get; set; } /* Armazena o caminho da string */
+
+    /* Para relacionar, precisamos incluir uma propriedade de navegação que indica
+        que categoria pode ter uma coleção de produtos
+     */
+     public ICollection<Produto>? Produtos {get; set;} // Isso já seria o suficente
 }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -9,4 +9,12 @@ public class Produto
     public string? ImageUrl { get; set; }
     public float  Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
+
+    /* Precisamos relacionar, dizendo que essa tabela vai ter a chave estrangeira
+        de uma categoria.
+        E precisamos de uma propriedade de navegação para indica que Produto se relaciona
+         com uma categoria.
+     */
+     public int CategoriaId {get; set;} // Mapeia a chave estrangeira
+     public Categoria? Categoria {get; set;} // Propeirdade de navegação que indica relacionamento
 }
