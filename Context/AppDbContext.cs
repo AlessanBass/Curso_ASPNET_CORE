77e27c2999ca506c943cb9838ffdc3a8763f24b7
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-04-06 10:58:06 -0300

    Criando a classe de contexto

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+/* Classe responsavel por realizar a comunição entre as minhas entidades
+    e o banco de dados relacional.
+ */
+public class AppDbContext : DbContext
+{
+    /* Atalho ctor cria um construtor */
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    /* 
+        Deixando a propriedade como nulable, podemos garanntir que a propriedade
+         seja opcional
+     */
+    public DbSet<Categoria> Categorias {get; set;}
+    public DbSet<Produto> Produtos {get; set;}
+
+}
