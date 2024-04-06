9fa718ace6b17731ddf53c1967f1e243651bc8c9
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-04-06 10:24:08 -0300

    Criando o modelo de domínio

---
+++ Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalogo.Models;
+
+public class Categoria
+{
+    /* Para chave primário ao usar EF usar nomes específicos */
+    public int CategoriaId {get; set;}
+    public string?  Nome { get; set; }
+    public string? ImageUrl { get; set; } /* Armazena o caminho da string */
+}
