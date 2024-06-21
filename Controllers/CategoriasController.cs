c3ac7f770d0fac02e92b2bb3791e4141399ca198
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-06-21 12:47:15 -0300

    MApenado as entidades

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.DTOs;
 using APICatalogo.Models;
 using APICatalogo.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -21,74 +22,122 @@ public class CategoriasController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Categoria>>> GetAll(){
+    public async Task<ActionResult<IEnumerable<CategoriaDTO>>> GetAll()
+    {
         var categorias = await _uof.CategoriaRepository.GetCategorias();
-        if(categorias is null){
+        if (categorias is null)
+        {
             return NotFound("Nenhuma categoria encontrada!");
         }
 
         return Ok(categorias); /* Retorna um 200 */
     }
 
-    [HttpGet("id", Name ="ObterCategoria")]
-    public async Task<ActionResult<Categoria>> GetOneId(int id){
+    [HttpGet("id", Name = "ObterCategoria")]
+    public async Task<ActionResult<CategoriaDTO>> GetOneId(int id)
+    {
         var categoria = await _uof.CategoriaRepository.GetCategoria(id);
 
-         if(categoria is null){
+        if (categoria is null)
+        {
             return NotFound("Categoria não encontrada!");
         }
 
-        return Ok(categoria); /* Retorna um 200 */
+        /* Antes de retorna devo fazer o mapeamento do DTO que será retornado */
+        var categoriaDTO = new CategoriaDTO()
+        {
+            CategoriaId = categoria.CategoriaId,
+            ImageUrl = categoria.ImageUrl,
+            Nome = categoria.Nome
+        };
+
+        return Ok(categoriaDTO); /* Retorna um 200 */
     }
 
-   /*  [HttpGet("produtos/id_categoria")]
-    public ActionResult<IEnumerable<Categoria>> GetAllCategoriaProduto(int id_categoria){
-        var produtosPorCategoria = _context.Categorias.Include(p=> p.Produtos).Where(c=> c.CategoriaId == id_categoria).ToList();
+    /*  [HttpGet("produtos/id_categoria")]
+     public ActionResult<IEnumerable<Categoria>> GetAllCategoriaProduto(int id_categoria){
+         var produtosPorCategoria = _context.Categorias.Include(p=> p.Produtos).Where(c=> c.CategoriaId == id_categoria).ToList();
 
-        if(produtosPorCategoria is null){
-            return NotFound("Pordutos não encontrados!");
-        }
+         if(produtosPorCategoria is null){
+             return NotFound("Pordutos não encontrados!");
+         }
 
-        return produtosPorCategoria;
+         return produtosPorCategoria;
 
-    } */
+     } */
 
     [HttpPost]
-    public async Task<ActionResult> Post(Categoria categoria){
-        if(categoria is null){
+    public async Task<ActionResult<CategoriaDTO>> Post(CategoriaDTO categoriaDTO)
+    {
+        if (categoriaDTO is null)
+        {
             return BadRequest();
         }
-        //Precisamos incluir o produto no contexto
-        //_context.Add(categoria);
-       // _context.SaveChanges(); //Persiste os dados na tabela
-       var redultado = await _uof.CategoriaRepository.Create(categoria);
-       await _uof.SaveChangesAsync();
+        /* Agora precisamos converter para uma entidade */
+        var categoria = new Categoria()
+        {
+            CategoriaId = categoriaDTO.CategoriaId,
+            ImageUrl = categoriaDTO.ImageUrl,
+            Nome = categoriaDTO.Nome
+        };
+        var redultado = await _uof.CategoriaRepository.Create(categoria);
+        await _uof.SaveChangesAsync();
+        var novaCaegoria = new CategoriaDTO()
+        {
+            CategoriaId = redultado.CategoriaId,
+            ImageUrl = redultado.ImageUrl,
+            Nome = redultado.Nome
+        };
 
 
-        return new CreatedAtRouteResult("ObterCategoria", new {id = redultado.CategoriaId}, redultado);
+
+        return new CreatedAtRouteResult("ObterCategoria", new { id = novaCaegoria.CategoriaId }, novaCaegoria);
     }
 
     [HttpPut("id")]
-    public async Task<ActionResult> Put(int id, Categoria categoria){
-        if(id != categoria.CategoriaId){
+    public async Task<ActionResult<CategoriaDTO>> Put(int id, CategoriaDTO categoriaDTO)
+    {
+        if (id != categoriaDTO.CategoriaId)
+        {
             return BadRequest();
         }
+        var categoria = new Categoria()
+        {
+            CategoriaId = categoriaDTO.CategoriaId,
+            ImageUrl = categoriaDTO.ImageUrl,
+            Nome = categoriaDTO.Nome
+        };
 
-       await _uof.CategoriaRepository.Update(categoria);
-       await _uof.SaveChangesAsync();
-        return Ok(categoria);
+        var retorno = await _uof.CategoriaRepository.Update(categoria);
+        await _uof.SaveChangesAsync();
+
+        var categoriaAtualizada = new CategoriaDTO{
+            CategoriaId = retorno.CategoriaId,
+            ImageUrl = retorno.ImageUrl,
+            Nome = retorno.Nome
+        };
+
+        return Ok(categoriaAtualizada);
     }
 
     [HttpDelete("id")]
-    public async Task<ActionResult> Delete (int id){
+    public async Task<ActionResult<CategoriaDTO>> Delete(int id)
+    {
         var categoria = await _uof.CategoriaRepository.GetCategoria(id);
-        if(categoria is null){
+        if (categoria is null)
+        {
             return NotFound("Categoria não encontrado");
         }
 
-       await _uof.CategoriaRepository.Delete(id);
-       await _uof.SaveChangesAsync();
+        var retorno = await _uof.CategoriaRepository.Delete(id);
+        await _uof.SaveChangesAsync();
 
-        return Ok(categoria);
+        var categoriaDeletada = new CategoriaDTO{
+            CategoriaId = retorno.CategoriaId,
+            ImageUrl = retorno.ImageUrl,
+            Nome = retorno.Nome
+        };
+
+        return Ok(categoriaDeletada);
     }
 }
